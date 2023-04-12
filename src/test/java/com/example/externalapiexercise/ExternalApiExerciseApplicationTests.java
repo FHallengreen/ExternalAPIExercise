0e56b4b80755ae8abc8c729f317d36dbaf87855d
FHallengreen <fred70p9@stud.kea.dk>
2023-04-12 21:05:17 +0200

    added caching

--- src/test/java/com/example/externalapiexercise/ExternalApiExerciseApplicationTests.java
+++ src/test/java/com/example/externalapiexercise/ExternalApiExerciseApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ExternalApiExerciseApplicationTests {
 
-    @Test
+/*    @Test
     void contextLoads() {
-    }
+    }*/
 
 }
